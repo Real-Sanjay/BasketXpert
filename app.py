4d0de1552ef09bf3e06000b0564eca2f15975772
Sanjay <139987767+Real-Sanjay@users.noreply.github.com>
2023-07-23 20:14:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.express as px
+# import plotly.express as px
 from mlxtend.preprocessing import TransactionEncoder
 from mlxtend.frequent_patterns import fpgrowth, association_rules
 
